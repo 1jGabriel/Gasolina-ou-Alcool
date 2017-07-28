05f7f0b9285a4ba52040df7e068206d8087f18ec
1jGabriel <gsantana152@gmail.com>
2017-07-27 22:14:15 -0300

    melhorias no codigo, alinhamento de caixas de texto

--- app/src/main/java/android/projetos/com/gasolinaoualcool/MainActivity.java
+++ app/src/main/java/android/projetos/com/gasolinaoualcool/MainActivity.java
@@ -1,48 +1,61 @@
 package android.projetos.com.gasolinaoualcool;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     private EditText precoAlcool;
-    private EditText precoGasolina;
-    private Button verificar;
-    private TextView resultado;
+    private  EditText precoGasolina;
+    private Button botaoVerificar;
+    private TextView resultadoTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Recuperar itens de tela
         precoAlcool = (EditText) findViewById(R.id.precoAlcoolId);
-        precoGasolina = (EditText) findViewById(R.id.precoAlcoolId);
-        resultado = (TextView) findViewById(R.id.precoAlcoolId);
+        precoGasolina = (EditText) findViewById(R.id.precoGasolinaId);
+        botaoVerificar = (Button) findViewById(R.id.buttonResultadoId);
+        resultadoTexto = (TextView) findViewById(R.id.textoResultadoId);
 
-        verificar.setOnClickListener(new View.OnClickListener() {
+        //adicionar evento clique
+        botaoVerificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Double valorAlcool = Double.parseDouble(precoAlcool.getText().toString());
-                Double valorGasolina = Double.parseDouble(precoGasolina.getText().toString());
+                //recupera valores digitados
+                String textoPrecoAlcool = precoAlcool.getText().toString();
+                String textoPrecoGasolina = precoGasolina.getText().toString();
 
-                resultadoConta(valorAlcool, valorGasolina);
+                //Converte valores string para numeros
+                Double valorAlcool = Double.parseDouble( textoPrecoAlcool );
+                Double valorGasolina = Double.parseDouble( textoPrecoGasolina );
+
+                //faz o calculo
+                double resultado = valorAlcool / valorGasolina;
+                resposta(resultado);
+
 
             }
-
         });
+
 }
 
-public void resultadoConta(double valorAlcool, double valorGasolina){
-    if(valorAlcool/valorGasolina >= 0.7){
-        resultado.setText("Melhor usar gasolina.");
-    }else{
-        resultado.setText("Melhor usar alcool.");
+    private void resposta(double resultado) {
+        if( resultado <= 0.7 ){
+            resultadoTexto.setText("É melhor utilizar Alcool ");
+        }else{
+            resultadoTexto.setText("É melhor utilizar Gasolina ");
+        }
     }
-}
+
 
 }
 
